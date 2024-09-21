caeee0f28bde945e05fbdaeb075e36026191d7d1
MIGUEL-MOTTA-U <miguelangelmu2016@gmail.com>
2024-09-21 12:47:00 -0500

    Pruebas

--- products/src/test/java/edu/eci/cvds/parcial/miguel/products/ProductManagerTest.java
+++ products/src/test/java/edu/eci/cvds/parcial/miguel/products/ProductManagerTest.java
@@ -1,7 +1,18 @@
 package edu.eci.cvds.parcial.miguel.products;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class ProductManagerTest {
+    @Autowired
+    private ProductManager productManager;
+    @BeforeEach
+    void setUp() {
+
+    }
     @Test///
     void contextLoads() {
     }
