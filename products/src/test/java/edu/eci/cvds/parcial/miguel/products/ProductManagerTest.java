cc78eb3f955aae5c90101b85e38b3acfa510bf1d
unknown <miguel.motta-u@labinfo.is.escuelaing.edu.co>
2024-09-21 12:48:46 -0500

    Add

--- products/src/test/java/edu/eci/cvds/parcial/miguel/products/ProductManagerTest.java
+++ products/src/test/java/edu/eci/cvds/parcial/miguel/products/ProductManagerTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.util.ArrayList;
 @SpringBootTest
 class ProductManagerTest {
     @Autowired
     private ProductManager productManager;
     @BeforeEach
     void setUp() {
-
+    productManager = new ProductManager(new ArrayList<>());
     }
     @Test///
     void contextLoads() {
